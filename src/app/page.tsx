346d7cf995654107c79c67007b5b7e222fe47eed
june9999 <vesper0@163.com>
2024-01-28 18:03:17 +0100

    fix warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               </div>
 
               <div className="hero-welcome-bio md:pl-[2rem] md:pt-[3rem] overflow-hidden">
-                <h3>My Languages and Tools:</h3>
+                <h1>My Languages and Tools:</h1>
                 <div className="flex w-[300%]  gap-[2px] linerani flex-wrap">
                   <Link
                     href="https://aws.amazon.com"
@@ -108,250 +108,258 @@ export default function Home() {
                       height="65"
                     />
                   </Link>
-                  {/* <a
+                  {/* <Link
                     href="https://getbootstrap.com"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                   <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
                       alt="bootstrap"
                       width="65"
                       height="65"
                     />
-                  </a> */}
-                  <a
+                  </Link> */}
+                  <Link
                     href="https://www.w3schools.com/css/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
                       alt="css3"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.docker.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
                       alt="docker"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://expressjs.com"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
                       alt="express"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.figma.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
                       alt="figma"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://git-scm.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
                       alt="git"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.w3.org/html/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
                       alt="html5"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.java.com"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
                       alt="java"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
                       alt="javascript"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://kubernetes.io"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg"
                       alt="kubernetes"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.mongodb.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
                       alt="mongodb"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.mysql.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
                       alt="mysql"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://nestjs.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg"
                       alt="nestjs"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://nextjs.org/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg"
                       alt="nextjs"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a href="https://nodejs.org" target="_blank" rel="noreferrer">
-                    <img
+                  </Link>
+                  <Link
+                    href="https://nodejs.org"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
                       alt="nodejs"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.postgresql.org"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
                       alt="postgresql"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://postman.com"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
                       alt="postman"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://reactjs.org/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
                       alt="react"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://tailwindcss.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
                       alt="tailwind"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="https://www.typescriptlang.org/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
                       alt="typescript"
                       width="65"
                       height="65"
                     />
-                  </a>
-                  <a href="https://vuejs.org/" target="_blank" rel="noreferrer">
-                    <img
+                  </Link>
+                  <Link
+                    href="https://vuejs.org/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
                       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg"
                       alt="vuejs"
                       width="65"
                       height="65"
                     />
-                  </a>
+                  </Link>
                 </div>
               </div>
             </Col>
