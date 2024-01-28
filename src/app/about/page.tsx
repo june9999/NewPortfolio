817c5cc3c77d336024466eebc8dff9aede24b606
june9999 <vesper0@163.com>
2024-01-28 10:24:31 +0100

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -40,27 +40,21 @@ const page = () => {
         <Col md="6">
           <div className={`${createFadeInClass()}`}>
             <p>My name is Wenjun Xue and I am a junior software engineer.</p>
-            <p>
+            <p className="my-[1rem]">
               After years of study, I have progressively gained a deeper
-              understanding of my nature and am now pursuing an alternative path
-              than I had previously anticipate. Without having to demonstrate my
-              academic prowess, I work to broaden my practical experience. I
-              have worked on a variety of technologies and projects up to this
-              point.
+              understanding of my nature and am now pursuing a path as software
+              engineer. Now I work to broaden my practical experience.
             </p>
 
-            <p>
-              Before I worked on a number of team projects throughout WBS
-              bootcamp. Using the Servelss framework and Typerscript, I was
-              instrumental in developing the technical MVP for the Xfactor tool
-              during my internship at Digital Product School. I am finding it
-              enjoyable to experiment with new technology and learn new things.
-              I now created my own website on my own, focusing on improving user
-              experiences and looking forward to the next adventure.
+            <p className="my-[1rem]">
+              Sofar I have worked on a variety of technologies and projects up
+              to this point. Throughout WBS bootcamp, I attended lots of group
+              work. During DPS(digital product school), my team and I developed
+              the technical MVP for the Xfactor tool. I now creating websites on
+              my own.
             </p>
             <p>
-              {`If you have any questions, ideas that you are possionate about, or
-              just want to connect, don&rsquo; hesitate to reach out. I&rsquo;
+              {`I am finding very exciting this journey with new technologies continuously. If you have any questions, ideas that you are possionate about, or just want to connect, don&rsquo; hesitate to reach out. I&rsquo;
               always open to collaboration.`}
             </p>
           </div>
