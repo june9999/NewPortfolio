d79f35fa98c292026e33639f6bb168a3639e6f73
june9999 <vesper0@163.com>
2024-01-28 13:37:40 +0100

    fix

--- src/app/portfolios/create/action.tsx
+++ src/app/portfolios/create/action.tsx
@@ -5,15 +5,17 @@ import { createPortfolio } from "@/lib/portfolios";
 export const submitPortfolioForm = (formdata: FormData) => {
   const today = Date.now;
   const rawformdata: portfolioData = {
-    title: (formdata.get("title") as string) || "fs",
-    type: (formdata.get("type") as string) || "fs",
-    content: (formdata.get("content") as string) || "fs",
-    status: (formdata.get("status") as string) || "fs",
-    techstack: ([formdata.get("techstack")] as string[]) || ["ds"],
-    imgUrl: "img",
+    title: (formdata.get("title") as string) || "undefined",
+    type: (formdata.get("type") as string) || "undefined",
+    content: (formdata.get("content") as string) || "undefined",
+    status: (formdata.get("status") as string) || "undefined",
+    techstack: ([formdata.get("techstack")] as string[]) || ["undefined"],
+    imgUrl: (formdata.get("imgUrl") as string) || "undefined",
     startDate: new Date(),
     endDate: new Date(),
-    githubLink: "github",
+    githubLink: formdata.get("githubLink") as string,
+    productLink: (formdata.get("productLink") as string) || "undefined",
+    other: formdata.get("other") as string,
   };
   createPortfolio(rawformdata);
 };
